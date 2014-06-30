50f9cf65f7dad42c7257c0c17d3397049f4ddebc
Emmanuel Bernard <emmanuel@hibernate.org>
2014-06-30 10:49:57 +0200

    HHH-9265 Make EntityEntryFactory a SessionFactoryServiceRegistry service

--- hibernate-core/src/main/java/org/hibernate/service/internal/StandardSessionFactoryServiceInitiators.java
+++ hibernate-core/src/main/java/org/hibernate/service/internal/StandardSessionFactoryServiceInitiators.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.hibernate.engine.internal.EntityEntryFactoryInitiator;
 import org.hibernate.engine.spi.CacheInitiator;
 import org.hibernate.event.service.internal.EventListenerServiceInitiator;
 import org.hibernate.service.spi.SessionFactoryServiceInitiator;
@@ -47,6 +48,7 @@ public class StandardSessionFactoryServiceInitiators {
 		serviceInitiators.add( EventListenerServiceInitiator.INSTANCE );
 		serviceInitiators.add( StatisticsInitiator.INSTANCE );
 		serviceInitiators.add( CacheInitiator.INSTANCE );
+		serviceInitiators.add( EntityEntryFactoryInitiator.INSTANCE );
 
 		return Collections.unmodifiableList( serviceInitiators );
 	}
